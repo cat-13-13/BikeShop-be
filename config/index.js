3056c3bddcbc0e483f35c46333ecc69af659bc5f
cfernandez <cfernandez@zzdatalabs.com>
2025-04-13 21:07:58 +0200

    refactor CORS configuration to allow multiple origins

--- config/index.js
+++ config/index.js
@@ -5,16 +5,32 @@ const cors = require("cors");
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:3000";
 
+const allowedOrigins = [
+  "https://markus-bikes-shop.vercel.app",
+  "http://localhost:3000",
+  "http://localhost:5173"
+];
+
 module.exports = (app) => {
   app.set("trust proxy", 1);
-  app.use(
-    cors({
-      origin: [FRONTEND_URL]
-    })
-  );
+  
 
-  app.use(logger("dev"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
+    app.use(cors({
+        origin: function (origin, callback) {
+            // Permitir peticiones sin origin (como las de curl o postman)
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                console.log("🔒 Bloqueado por CORS:", origin);
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true
+    }));
+
+
+    app.use(logger("dev"));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
 };
